e6bce000bf363f7421b8dd8847f84b341c84fac5
Alex Mastro <amastro@fb.com>
2022-03-06 18:25:38 -0800

    Switch to lua config

--- setup.sh
+++ setup.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-# .vimrc
-if [ -f ~/.vimrc ]; then
-  mv ~/.vimrc{,.bak}
-fi
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-
-# .tmux.conf
-if [ -f ~/.tmux.conf ]; then
-  mv ~/.tmux.conf{,.bak}
-fi
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-
-# neovim
-if [ -d ~/.config/nvim ]; then
-  mv ~/.config/nvim{,.bak}
-else
-  mkdir -p ~/.config
-fi
+mkdir -p ~/.config
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
